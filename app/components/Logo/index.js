a17fa1b49a0586f55017c4c36d91e960f6258f50
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-15 16:18:34 +0100

    Change logo

--- app/components/Logo/index.js
+++ app/components/Logo/index.js
@@ -1,8 +1,11 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./styles.scss";
 
 const Logo = ({title}) => (
-  <h1 className="Logo">{title}</h1>
+  <h1 className="Logo">
+    <img className="Logo__Image" src={logo} alt="Mickael Zhang"/>
+  </h1>
 );
 
 export default Logo;
