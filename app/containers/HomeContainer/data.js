d8419100b028f7e94161cc86cfda59d4ac59c84f
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-18 18:51:15 +0100

    Separate data from component state

---
+++ app/containers/HomeContainer/data.js
@@ -0,0 +1,45 @@
+const DATA = {
+  title: 'Mickael Zhang',
+  email: 'mickael.zhg@gmail.com',
+  github: '//github.com/mickaelzhang',
+  description: `
+    French student at Hetic looking for an summer intership in FrontEnd
+    Development from July to October. Feel free to contact me.
+  `,
+  projects: [
+    {
+      name: 'Vaïvaï',
+      slug: 'vaivai',
+      type: 'WordPress Theme Development',
+      url: '//www.vaivai.fr',
+      isDisplayed: true
+    },
+    {
+      name: 'Julien Liénard',
+      slug: 'julien-lienard',
+      type: 'WordPress Theme Development',
+      url: '//www.julienlienard.net/',
+      isDisplayed: true
+    },
+    {
+      name: 'Packshot Creator',
+      slug: 'packshot-creator',
+      type: 'Front-End Development',
+      url: '//packshot-creator.com',
+      isDisplayed: true
+    },
+    {
+      name: 'Parme Avocats',
+      slug: 'parme-avocats',
+      type: 'Front-End Development',
+      url: '//www.parme-avocats.com',
+      isDisplayed: true
+    },
+  ],
+  returnToTop: {
+    isActive: false
+  },
+  imgPath: '/dist/images/'
+};
+
+export default DATA;
