102ba2494173703b2d699c71f22e4e6ffead3aec
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-19 00:18:55 +0100

    Change project order

--- app/containers/HomeContainer/data.js
+++ app/containers/HomeContainer/data.js
@@ -10,32 +10,28 @@ const DATA = {
   `,
   projects: [
     {
-      name: 'Vaïvaï',
-      slug: 'vaivai',
-      type: 'WordPress Theme Development',
-      url: '//www.vaivai.fr',
-      isDisplayed: true
-    },
-    {
       name: 'Julien Liénard',
       slug: 'julien-lienard',
       type: 'WordPress Theme Development',
-      url: '//www.julienlienard.net/',
-      isDisplayed: true
+      url: '//www.julienlienard.net/'
+    },
+    {
+      name: 'Vaïvaï',
+      slug: 'vaivai',
+      type: 'WordPress Theme Development',
+      url: '//www.vaivai.fr'
     },
     {
       name: 'Packshot Creator',
       slug: 'packshot-creator',
       type: 'Front-End Development',
-      url: '//packshot-creator.com',
-      isDisplayed: true
+      url: '//packshot-creator.com'
     },
     {
       name: 'Parme Avocats',
       slug: 'parme-avocats',
       type: 'Front-End Development',
-      url: '//www.parme-avocats.com',
-      isDisplayed: true
+      url: '//www.parme-avocats.com'
     },
   ],
   returnToTop: {
