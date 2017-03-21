44f8ba3d602cbe6f1923d3f57397e8ac4cc55828
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-21 17:08:47 +0100

    Change home description

--- src/containers/HomeContainer/data.js
+++ src/containers/HomeContainer/data.js
@@ -4,9 +4,9 @@ const DATA = {
   github: '//github.com/mickaelzhang',
   description: `
     French student at Hetic and freelancer in my free time. I love learning
-    things related to development even though I'm specilized in frontend
-    development. I inform myself on backend and devops as well. Currently looking
-    for a 4 months internship. Feel free to contact me for any inquiries !
+    things related to web development. Even though I'm focus on front-end,
+    I enjoy learning more on backend and devops stuff as well. I'm currently
+    looking for a 4 months internship. Feel free to contact me for any inquiries !
   `,
   projects: [
     {
