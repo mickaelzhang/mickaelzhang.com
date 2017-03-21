63adc083ed9d438efc4b8759b14b2c2e5f8eac88
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-21 01:14:29 +0100

    Add routing to the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import "@shared/styles/general.scss";
 
 import HomeContainer from "@containers/HomeContainer";
 
-
 render(
-  <HomeContainer />,
+  <Router>
+      <Switch>
+        <Route exact path="/" component={HomeContainer}/>
+        <Route path="/projects/:slug" render={HomeContainer}/>
+        <Route render={() => (
+          <div style={{color: 'white'}}>404</div>
+        )}/>
+      </Switch>
+  </Router>,
   document.getElementById("root")
 );
