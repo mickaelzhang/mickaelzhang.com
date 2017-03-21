9dc0a98fd4286d19b4e9e17fe1f1e4f6130f2de5
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-21 02:38:41 +0100

    Change BrowserRouter to HasRouter
    Server can't run nginx right now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import "@shared/styles/general.scss";
 
