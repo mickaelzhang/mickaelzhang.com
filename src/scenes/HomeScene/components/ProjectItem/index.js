bfb4c7a288b3c7a4bbcc78c18146eec598279c18
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-22 15:17:39 +0100

    Change all project visual

--- src/scenes/HomeScene/components/ProjectItem/index.js
+++ src/scenes/HomeScene/components/ProjectItem/index.js
@@ -56,7 +56,7 @@ class ProjectItem extends Component {
 
   render() {
     const { name, type, url, slug } = this.props;
-    const imgPath = require(`@shared/images/${slug}_visual.png`);
+    const imgPath = require(`@shared/images/${slug}_visual.jpg`);
     const projectClass = classNames("ProjectItem", {
       "ProjectItem--displayed": this.state.isDisplayed,
       "ProjectItem--above": this.state.isAbove,
