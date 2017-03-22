e60047b6032465636514c05ae38ea01117f4e70f
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-23 00:55:44 +0100

    Add project screenshot

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,21 +18,7 @@ const projects = [
     slug: 'vaivai',
     type: 'WordPress Theme Development',
     url: '//www.vaivai.fr',
-    previewNumber: 5,
-    description: `
-      Ideoque fertur neminem aliquando ob haec vel similia poenae addictum
-      oblato de more elogio revocari iussisse, quod inexorabiles quoque
-      principes factitarunt. et exitiale hoc vitium, quod in aliis non numquam
-      intepescit, in illo aetatis progressu effervescebat, obstinatum eius
-      propositum accendente adulatorum cohorte.
-    `
-  },
-  {
-    name: 'Packshot Creator',
-    slug: 'packshot-creator',
-    type: 'Front-End Development',
-    url: '//packshot-creator.com',
-    previewNumber: 5,
+    previewNumber: 4,
     description: `
       Ideoque fertur neminem aliquando ob haec vel similia poenae addictum
       oblato de more elogio revocari iussisse, quod inexorabiles quoque
@@ -46,7 +32,7 @@ const projects = [
     slug: 'parme-avocats',
     type: 'Front-End Development',
     url: '//www.parme-avocats.com',
-    previewNumber: 5,
+    previewNumber: 3,
     description: `
       Ideoque fertur neminem aliquando ob haec vel similia poenae addictum
       oblato de more elogio revocari iussisse, quod inexorabiles quoque
