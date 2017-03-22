fc85701fef05dc85bc49766601a0b515eee0b458
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-22 02:05:52 +0100

    Fix email link

--- src/scenes/HomeScene/components/AboutMe/index.js
+++ src/scenes/HomeScene/components/AboutMe/index.js
@@ -5,7 +5,7 @@ const AboutMe = ({text, email, github}) => (
   <div className="AboutMe">
     <div className="AboutMe__Text">{text}</div>
     <div className="AboutMe__ContactInfo">
-      <a className="AboutMe__Link" href={github}>Github</a> / <a className="AboutMe__Link" href={email}>Email</a>
+      <a className="AboutMe__Link" href={github}>Github</a> / <a className="AboutMe__Link" href={`mailto:${email}`}>Email</a>
     </div>
   </div>
 );
