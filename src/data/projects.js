41bee6185632d2426ba516990053c35fe8e939e7
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-23 01:50:02 +0100

    Change project description

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,11 +6,8 @@ const projects = [
     url: '//www.julienlienard.net/',
     previewNumber: 5,
     description: `
-      Ideoque fertur neminem aliquando ob haec vel similia poenae addictum
-      oblato de more elogio revocari iussisse, quod inexorabiles quoque
-      principes factitarunt. et exitiale hoc vitium, quod in aliis non numquam
-      intepescit, in illo aetatis progressu effervescebat, obstinatum eius
-      propositum accendente adulatorum cohorte.
+      Julien Liénard is a french photographer who does mainly portrait. Working
+      with the agency Escabo, we created his portfolio which present his works.
     `
   },
   {
@@ -20,11 +17,9 @@ const projects = [
     url: '//www.vaivai.fr',
     previewNumber: 4,
     description: `
-      Ideoque fertur neminem aliquando ob haec vel similia poenae addictum
-      oblato de more elogio revocari iussisse, quod inexorabiles quoque
-      principes factitarunt. et exitiale hoc vitium, quod in aliis non numquam
-      intepescit, in illo aetatis progressu effervescebat, obstinatum eius
-      propositum accendente adulatorum cohorte.
+      Vaïvaï is a drink company that mainly produce coconut milk. Present mainly
+      in France, they distribute their drink in most chainstore such as
+      Carrefour, Monoprix or Auchan.
     `
   },
   {
@@ -34,11 +29,9 @@ const projects = [
     url: '//www.parme-avocats.com',
     previewNumber: 3,
     description: `
-      Ideoque fertur neminem aliquando ob haec vel similia poenae addictum
-      oblato de more elogio revocari iussisse, quod inexorabiles quoque
-      principes factitarunt. et exitiale hoc vitium, quod in aliis non numquam
-      intepescit, in illo aetatis progressu effervescebat, obstinatum eius
-      propositum accendente adulatorum cohorte.
+      Founded in 1999, Parme avocats is one of France's leading law firm.
+      Wanting to modernize their visual identity, they choose to make a brand
+      new website.
     `
   },
 ];
