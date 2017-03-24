0b485f8ff4500c55f4ebab7af1d22011cb8e8b5d
Mickael Zhang <mickael.zhg@gmail.com>
2017-03-25 00:49:04 +0100

    Fix typo

--- src/data/home.js
+++ src/data/home.js
@@ -4,9 +4,10 @@ const home = {
   github: '//github.com/mickaelzhang',
   description: `
     French student at Hetic and freelancer in my free time. I love learning
-    things related to web development. Even though I'm focus on front-end,
+    things related to web development. Even though I focus on front-end,
     I enjoy learning more on backend and devops stuff as well. I'm currently
-    looking for a 4 months internship. Feel free to contact me for any inquiries !
+    looking for a 4 month internship in front-end development. Feel free to
+    contact me for any inquiries !
   `
 };
 
