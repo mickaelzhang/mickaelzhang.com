77500b2b11baf61062fdb7027892815b7dc5ed0b
Mickael Zhang <mickael.zhg@gmail.com>
2017-04-25 17:07:42 +0200

    Change home page description

--- src/data/home.js
+++ src/data/home.js
@@ -7,7 +7,7 @@ const home = {
     things related to web development. Even though I focus on front-end,
     I enjoy learning more on backend and devops stuff as well. I'm currently
     looking for a 4 month internship in front-end development. Feel free to
-    contact me for any inquiries !
+    contact me for any inquiries.
   `
 };
 
