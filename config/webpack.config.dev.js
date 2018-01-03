d1932073c7c04726f53cde5c287410df339b6345
Mickael Zhang <mickael.zhg@gmail.com>
2018-01-03 16:49:28 +0100

    Add fonts

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -193,6 +193,13 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.(eot|ttf)/,
+            loader: "file-loader",
+            options: {
+              name: "static/fonts/[name].[ext]",
+            },
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -212,6 +212,13 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.(eot|ttf)/,
+            loader: "file-loader",
+            options: {
+              name: "static/fonts/[name].[ext]",
+            },
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader don't uses a "test" so it will catch all modules
