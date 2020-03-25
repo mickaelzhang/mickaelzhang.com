1428272881893f570736bae4361f31a1bce7981c
Mickael Zhang <mickael.zhg@gmail.com>
2020-03-25 23:23:13 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,22 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'react-hooks'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    quotes: ['error', 'single'],
+  },
 }
